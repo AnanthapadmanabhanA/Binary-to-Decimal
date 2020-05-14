ab5a1896f50a9460bdd0db4ea5227d5f3223288f
Ananthu <ananthucbz@gmail.com>
2020-05-14 20:46:09 +0530

    Add files via upload

---
+++ binarytodecimal.c
@@ -0,0 +1,37 @@
+//
+//  binarytodecimal.c
+//  
+//
+//  Created by Ananthu on 14/05/20.
+//
+
+#include "binarytodecimal.h"
+
+#include <stdio.h>
+#include <math.h>
+int binaryToDecimal(long binarynum)
+{
+    
+int decimalnum = 0, temp = 0, remainder;
+while (binarynum!=0)
+{
+remainder = binarynum % 10;
+binarynum = binarynum / 10;
+decimalnum = decimalnum + remainder*pow(2,temp);
+temp++;
+}
+return decimalnum;
+}
+
+int main()
+{
+long binarynum;
+printf("Enter a binary number: ");
+scanf("%ld", &binarynum);
+    
+printf("Equivalent decimal number is: %d",
+       
+binaryToDecimal(binarynum));
+ return 0;
+
+}
---
+++ binarytodecimal.h
@@ -0,0 +1,13 @@
+//
+//  binarytodecimal.h
+//  
+//
+//  Created by Ananthu on 14/05/20.
+//
+
+#ifndef binarytodecimal_h
+#define binarytodecimal_h
+
+#include <stdio.h>
+
+#endif /* binarytodecimal_h */
